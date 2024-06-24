3577b591c2793253a49e8b98725de786c3f9c3fe
John Pachkoski <jpachkoski@gmail.com>
2024-06-24 14:31:37 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { COLORS } from './colors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'colors-app';
+  colors = COLORS;
+
+  addColor(input: HTMLInputElement): void {
+    if (!input.value) {
+      return;
+    }
+
+    const newColor = { name: input.value, hex: Math.random().toString() };
+
+    this.colors = [...this.colors, newColor];
+    input.value = '';
+  }
+
+  colorTrackBy(index: any, color: any) {
+    return color.hex;
+  }
 }
